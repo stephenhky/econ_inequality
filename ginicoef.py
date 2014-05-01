ebdabb9b9d09b820dbdd239b1bb699fc051cc458
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2014-05-01 10:02:11 -0400

    simplified, vectorized routine to calculate the Gini coefficients

--- ginicoef.py
+++ ginicoef.py
@@ -27,3 +27,12 @@ class GiniCoefficientCalculator:
         B = self.area_under_curve(curve_points)
         A = 0.5 - B
         return A / (A+B)
+
+def gini_coef(wealths):
+    cum_wealths = np.cumsum(sorted(np.append(wealths, 0)))
+    sum_wealths = cum_wealths[-1]
+    xarray = np.array(range(0, len(cum_wealths))) / np.float(len(cum_wealths)-1)
+    yarray = cum_wealths / sum_wealths
+    B = np.trapz(yarray, x=xarray)
+    A = 0.5 - B
+    return A / (A+B)
